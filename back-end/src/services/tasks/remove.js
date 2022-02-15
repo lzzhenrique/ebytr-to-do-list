384656e8494b7c25c4b352633b96657d848e57d3
Luiz Henrique <luishenriqueee2010@hotmail.com>
2022-02-15 17:15:21 -0300

    feat: resolve syntax erros

--- back-end/src/services/tasks/remove.js
+++ back-end/src/services/tasks/remove.js
@@ -1,6 +1,6 @@
 const { remove } = require('../../models')('tasks');
 
 module.exports = async (id) => {
-  const deleteRecipe = await remove(id);
-  return deleteRecipe;
+  const deleteTask = await remove(id);
+  return deleteTask;
 };
