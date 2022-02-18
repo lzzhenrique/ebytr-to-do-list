abed96cdc382c085d9c13ac2110ed218e780d6f9
Luiz Henrique <luishenriqueee2010@hotmail.com>
2022-02-18 11:18:42 -0300

    add: test coverage %

---
+++ back-end/src/tests/utils/errRequests.js
@@ -0,0 +1,41 @@
+const emptyFields = [
+    {
+      description: 'Comsprar feijao e farinha',
+      createdAt: '2030-02-16',
+      deadline: '2033-05-19',
+      status: 'Pending',
+      userId: '619cf05c1b42550e2b16h2cf',
+    },
+    {
+      title: 'Ir ao mercafdso',
+      createdAt: '2020-02-16',
+      deadline: '2023-05-19',
+      status: 'Pending',
+      userId: '619cf05c1b42550e2b16h2cf',
+    },
+    {
+      title: 'Ir ao mercadso',
+      description: 'Comprar feijao e farinha',
+      deadline: '2023-05-19',
+      status: 'Pending',
+      userId: '619cf05c1b42550e2b16h2cf',
+    },
+    {
+      title: 'Ir ao mercadso',
+      description: 'Comprar feijao e farinha',
+      createdAt: '2020-02-16',
+      status: 'Pending',
+      userId: '619cf05c1b42550e2b16h2cf',
+    },
+    {
+      title: 'Ir ao mercadso',
+      description: 'Comprar feijao e farinha',
+      createdAt: '2020-02-16',
+      deadline: '2023-05-19',
+      userId: '619cf05c1b42550e2b16h2cf',
+    },
+];
+
+module.exports = {
+  emptyFields,
+};
