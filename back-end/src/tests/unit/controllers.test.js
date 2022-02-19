c32363ff502f939b7d5a23d89f4ba91062133461
Luiz Henrique <luishenriqueee2010@hotmail.com>
2022-02-19 10:17:34 -0300

    add: unit tests for find and update

--- back-end/src/tests/unit/controllers.test.js
+++ back-end/src/tests/unit/controllers.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines */
 /* eslint-disable sonarjs/cognitive-complexity */
 /* eslint-disable max-lines-per-function */
 const sinon = require('sinon');
@@ -22,6 +23,14 @@ const TasksControllers = require('../../controllers/tasks');
 const TASK = {
   title: 'Ir ao mercadso',
   description: 'Comprar feijao e farinha',
+  createdAt: '2020-02-16',
+  deadline: '2023-05-19',
+  status: 'Pending',
+};
+
+const TASK_TO_UPDATE = {
+  title: 'Ir afeira',
+  description: 'comprar abacate',
   createdAt: '2020-02-16',
   deadline: '2023-05-19',
   status: 'Pending',
@@ -91,7 +100,7 @@ describe('Testa a camada de controller para Tasks', () => {
           .returns();
   
         sinon.stub(TasksService, 'create')
-          .resolves({ error: 'BLALBALBLAL' });
+          .resolves();
       });
   
       after(() => {
@@ -138,9 +147,6 @@ describe('Testa a camada de controller para Tasks', () => {
           .returns();
         response.json = sinon.stub()
           .returns();
-  
-        sinon.stub(TasksService, 'remove')
-          .resolves(true);
       });
   
       it('é chamado o status com o código 204', async () => {
@@ -173,6 +179,153 @@ describe('Testa a camada de controller para Tasks', () => {
   
       it('Chama a função next', async () => {
         await TasksControllers.remove(request, response, next);
+        expect(countNextCalls).to.be.greaterThan(0);
+      });
+    });
+  });
+  describe('Testa controllers.find', () => {
+    describe('Testa procurar por uma task', () => {
+      describe('Retorna um erro quando a task nao existe', () => {
+        const response = {};
+        const request = {};
+        let countNextCalls = 0;
+        const next = (e) => {
+          if (e.message) countNextCalls += 1;
+        }; 
+    
+        before(async () => {
+          request.params = { id: ID };
+          request.user = { _id: '619cf05c1b42550e2b16h2cf' };
+    
+          response.status = sinon.stub()
+            .returns(response);
+          response.json = sinon.stub()
+            .returns();
+    
+          sinon.stub(TasksService, 'find')
+            .resolves(true);
+        });
+
+        after(() => {
+          TasksService.find.restore();
+        });
+    
+        it('é chamado o status com o código 204', async () => {
+          await TasksControllers.find(request, response, next);
+    
+          expect(countNextCalls).to.be.greaterThan(0);
+        });
+      });
+    });
+    describe('Testa procurar por todas as tasks', () => {
+      describe('Retorna todas as tasks do db', () => {
+        const response = {};
+        const request = {};
+        let countNextCalls = 0;
+        const next = (e) => {
+          if (e.message) countNextCalls += 1;
+        }; 
+    
+        before(async () => {
+          request.user = { _id: '619cf05c1b42550e2b16h2cf' };
+          request.params = { };
+    
+          response.status = sinon.stub()
+            .returns(response);
+          response.json = sinon.stub()
+            .returns([]);
+        });
+    
+        it('a funcao next nao eh chamada', async () => {
+          await TasksControllers.find(request, response, next);
+    
+          expect(countNextCalls).to.be.equals(0);
+        });
+        it('a funcao next nao eh chamada', async () => {
+          await TasksControllers.find(request, response, next);
+    
+          expect(response.json()).to.be.an('array');
+        });
+      });
+    });
+  });
+  describe('Testa controllers.update', () => {
+    describe('Quando a req enviada é válida', () => {
+      const response = {};
+      const request = {};
+  
+      before(() => {
+        request.body = { ...TASK_TO_UPDATE };
+        request.user = { _id: '619cf05c1b42550e2b16h2cf' };
+        request.params = { id: '62104e7c97252a6342d0c7ff' };
+  
+        response.status = sinon.stub()
+          .returns(response);
+        response.json = sinon.stub()
+          .returns({ TASK_TO_UPDATE });
+  
+        sinon.stub(TasksService, 'update')
+          .resolves(true);
+      });
+  
+      after(() => {
+        TasksService.update.restore();
+      });
+  
+      it('é chamado o status com o código 200', async () => {
+        await TasksControllers.update(request, response);
+  
+        expect(response.status.calledWith(200)).to.be.equal(true);
+      });
+  
+      it('é chamado o json com a mensagem "Filme criado com sucesso!"', async () => {
+        await TasksControllers.create(request, response);
+        const responseJSON = response.json();
+  
+        expect(responseJSON).to.be.deep.equal({ TASK_TO_UPDATE });
+      });
+    });
+    describe('Quando a req enviada nao tem os campos corretos', () => {
+      const response = {};
+      const request = {};
+      let countNextCalls = 0;
+      const next = (e) => {
+        if (e.message) countNextCalls += 1;
+      }; 
+  
+      before(() => {
+        request.body = {};
+        request.user = { _id: '619cf05c1b42550e2b16h2cf' };
+        request.params = { id: '62104e7c97252a6342d0c7ff' };
+  
+        response.status = sinon.stub()
+          .returns(response);
+        response.json = sinon.stub()
+          .returns();
+  
+        sinon.stub(TasksService, 'update')
+          .resolves();
+      });
+  
+      after(() => {
+        TasksService.update.restore();
+      });
+  
+      it('Chama a função next', async () => {
+        await TasksControllers.create(request, response, next);
+        expect(countNextCalls).to.be.greaterThan(0);
+      });
+    });
+    describe('Quando a req enviada não tem body ou user', () => {
+      const response = {};
+      const request = {};
+      let countNextCalls = 0;
+      const next = (e) => {
+        if (e.message) countNextCalls += 1;
+      }; 
+    
+      it('Chama a função next', async () => {
+        await TasksControllers.create(request, response, next);
         expect(countNextCalls).to.be.greaterThan(0);
       });
     });
